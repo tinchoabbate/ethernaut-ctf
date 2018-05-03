229c4c23a7da724ea2de70ab342bef646b2f2cc6
Martin Abbatemarco <mabbatemarco@deloitte.com>
2018-05-03 17:29:23 -0300

    Add Token exploit

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,15 +1,18 @@
-let Fallback = artifacts.require('./Fallback.sol');
-let Fallout = artifacts.require('./Fallout.sol');
-let CoinFlip = artifacts.require('./CoinFlip.sol');
-let CoinFlipAttack = artifacts.require('./CoinFlipAttack.sol');
-let Telephone = artifacts.require('./Telephone.sol');
-let TelephoneAttack = artifacts.require('./TelephoneAttack.sol');
+let Fallback = artifacts.require('./Fallback.sol')
+let Fallout = artifacts.require('./Fallout.sol')
+let CoinFlip = artifacts.require('./CoinFlip.sol')
+let CoinFlipAttack = artifacts.require('./CoinFlipAttack.sol')
+let Telephone = artifacts.require('./Telephone.sol')
+let TelephoneAttack = artifacts.require('./TelephoneAttack.sol')
+let Token = artifacts.require('./Token.sol')
+const TOKEN_INITIAL_SUPPLY = 20
 
 module.exports = deployer => {
-    deployer.deploy(Fallback);
-    deployer.deploy(Fallout);
-    deployer.deploy(CoinFlip);
-    deployer.deploy(CoinFlipAttack);
-    deployer.deploy(Telephone);
-    deployer.deploy(TelephoneAttack);
+    deployer.deploy(Fallback)
+    deployer.deploy(Fallout)
+    deployer.deploy(CoinFlip)
+    deployer.deploy(CoinFlipAttack)
+    deployer.deploy(Telephone)
+    deployer.deploy(TelephoneAttack)
+    deployer.deploy(Token, TOKEN_INITIAL_SUPPLY)
 }
